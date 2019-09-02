6e8baa389a2c770880292fedeab6f9819989e4ad
lautaro <lautaroecarro@gmail.com>
2019-09-02 19:53:13 -0300

    add project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Net_Core_With_GitHubActions.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
